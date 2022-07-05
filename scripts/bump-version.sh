3994f67fae4fbe5473bc04639779d7f2ca334e89
Aaron Kollasch <aaron@kollasch.dev>
2022-07-05 14:23:14 -0400

    Bump version: 2.81-beta.2

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,3 +5,5 @@ sed -i '' "s/\([_\s]\)VERSION = [-0-9A-Za-z.]*;$/\1VERSION = $CURRENT_PROJECT_VE
 sed -i '' "s/\([_\s]\)VERSION = [-0-9A-Za-z.]*;$/\1VERSION = $CURRENT_PROJECT_VERSION;/g" prefpane/Jitouch.xcodeproj/project.pbxproj
 sed -i '' "s/\"Version [-0-9A-Za-z.]*\"/\"Version $CURRENT_PROJECT_VERSION\"/g" prefpane/Base.lproj/JitouchPref.xib
 
+git add jitouch/Jitouch/Jitouch.xcodeproj/project.pbxproj prefpane/Base.lproj/JitouchPref.xib prefpane/Jitouch.xcodeproj/project.pbxproj
+git commit -m "Bump version: $CURRENT_PROJECT_VERSION"
