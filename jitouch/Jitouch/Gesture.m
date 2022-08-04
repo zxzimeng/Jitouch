9674cc51afcbb3e3e4483f40ae94ac4466385b3e
Aaron Kollasch <aaron@kollasch.dev>
2022-08-04 18:50:35 -0400

    Add left and right click to trackpad actions

--- jitouch/Jitouch/Gesture.m
+++ jitouch/Jitouch/Gesture.m
@@ -2729,6 +2729,14 @@ static CGEventRef CGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEve
                 simulatingByDevice = TRACKPAD;
                 CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 2);
                 CGEventSetType(event, kCGEventOtherMouseDown);
+            } else if ([command isEqualToString:@"Left Click"]) {
+                simulating = LEFTBUTTONDOWN;
+                CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 0);
+                CGEventSetType(event, kCGEventLeftMouseDown);
+            } else if ([command isEqualToString:@"Right Click"]) {
+                simulating = RIGHTBUTTONDOWN;
+                CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 1);
+                CGEventSetType(event, kCGEventRightMouseDown);
             } else if ([command isEqualToString:@"Open Link in New Tab"]) {
                 simulating = COMMANDANDLEFTBUTTONDOWN;
                 CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 0);
@@ -2748,6 +2756,14 @@ static CGEventRef CGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEve
                 simulatingByDevice = TRACKPAD;
                 CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 2);
                 CGEventSetType(event, kCGEventOtherMouseDown);
+            } else if ([command isEqualToString:@"Left Click"]) {
+                simulating = LEFTBUTTONDOWN;
+                CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 0);
+                CGEventSetType(event, kCGEventLeftMouseDown);
+            } else if ([command isEqualToString:@"Right Click"]) {
+                simulating = RIGHTBUTTONDOWN;
+                CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 1);
+                CGEventSetType(event, kCGEventRightMouseDown);
             } else if ([command isEqualToString:@"Open Link in New Tab"]) {
                 simulating = COMMANDANDLEFTBUTTONDOWN;
                 CGEventSetIntegerValueField(event, kCGMouseEventButtonNumber, 0);
