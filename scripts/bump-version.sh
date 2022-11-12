af49066f7bb4c0bbb5c2394c1071f3fae489cb3c
Aaron Kollasch <aaron@kollasch.dev>
2022-11-11 20:20:08 -0500

    Update bump-version script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 CURRENT_PROJECT_VERSION="$1"  # e.g. 2.75
 
-sed -i '' "s/\([_\s]\)VERSION = [-0-9A-Za-z.]*;$/\1VERSION = $CURRENT_PROJECT_VERSION;/g" jitouch/Jitouch/Jitouch.xcodeproj/project.pbxproj
-sed -i '' "s/\([_\s]\)VERSION = [-0-9A-Za-z.]*;$/\1VERSION = $CURRENT_PROJECT_VERSION;/g" prefpane/Jitouch.xcodeproj/project.pbxproj
-sed -i '' "s/\"Version [-0-9A-Za-z.]*\"/\"Version $CURRENT_PROJECT_VERSION\"/g" prefpane/Base.lproj/JitouchPref.xib
+sed -E -i '' "s/([_\s])VERSION = \"?[-0-9A-Za-z.]*\"?;$/\1VERSION = \"$CURRENT_PROJECT_VERSION\";/g" jitouch/Jitouch/Jitouch.xcodeproj/project.pbxproj
+sed -E -i '' "s/([_\s])VERSION = \"?[-0-9A-Za-z.]*\"?;$/\1VERSION = \"$CURRENT_PROJECT_VERSION\";/g" prefpane/Jitouch.xcodeproj/project.pbxproj
+sed -E -i '' "s/\"Version [-0-9A-Za-z.]*\"/\"Version $CURRENT_PROJECT_VERSION\"/g" prefpane/Base.lproj/JitouchPref.xib
 
 git add jitouch/Jitouch/Jitouch.xcodeproj/project.pbxproj prefpane/Base.lproj/JitouchPref.xib prefpane/Jitouch.xcodeproj/project.pbxproj
 git commit -m "Bump version: $CURRENT_PROJECT_VERSION"
